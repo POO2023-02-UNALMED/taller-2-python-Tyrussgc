a87596faed7e8f4126ae22266237b032ae2412b3
Tyrussgc <141864114+Tyrussgc@users.noreply.github.com>
2023-08-21 01:44:50 -0500

    clases

---
+++ main.py
@@ -0,0 +1,37 @@
+
+class Asientos:
+    def __init__(color, registro, precio):
+        self.color = color 
+        self.regitro = registro
+        self.precio = precio
+    
+    def cambiarColor(self, color):
+
+
+class Motor:
+    def __init__(numeroCilindros, tipo, registro):
+        self.numeroCilindros = numeroCilindros
+        self.tipo= tipo
+        self.registro = registro
+
+    def CambiarRegistro(self):
+
+    def asignarTipo(self):
+
+
+
+class Auto:
+    def __init__(modelo, precio, asientos, marca, motor, registro, cantidadCreados):
+        Self.modelo = modelo
+        Self.precio = precio
+        Self.asiento = [Asientos]
+        Self.marca = marca
+        Self.motor = Motor
+        Self.registro = registro
+        Self.cantidadCreados = cantidadCreados
+
+    def cantidadAsientos(self):
+
+
+    def verificarIntegridad(self):
+        
